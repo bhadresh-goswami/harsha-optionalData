fee17af7baa7c90a185976ff818b496099c41dc1
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-11 05:00:01 -0800

    done

--- optionalData/main.swift
+++ optionalData/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+
+//var a:Int
+//error: it is not set or it is nil
+//print("the value of a is \(a)")
+
+var a:Int?
+//? indicated that it is optional in case of no data it will display nil
+a = 10
+print("The value of a is \(a ?? 0)")
+var c = a! + 1
+
+var b:Int!
+//b += 1
+//var d = b + 1
+print("The value of b is \(b)")
 
